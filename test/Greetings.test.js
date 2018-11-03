bb0004c6dfe6c0e44fbb28982090868451e5d177
Kevin Hung <kevhung11@gmail.com>
2018-11-03 15:51:00 -0700

    Add files via upload

--- test/Greetings.test.js
+++ test/Greetings.test.js
@@ -4,13 +4,39 @@ const Web3 = require('web3');  // uppercase variable because used as a construct
 
 const web3 = new Web3(ganache.provider()); // Web3 Constructor creates instance
 
-beforeEach(() => {
-  web3.eth.getAccounts().then(fetchedAccounts => {
-    console.log(fetchedAccounts);
-  });
+const { interface, bytecode } = require('../compile'); // use ABI, *interface* and  *bytecode* properties of compiled smart contract object
+
+let accounts;
+let greetings
+
+beforeEach(async () => {
+
+  accounts = await web3.eth.getAccounts();
+  greetings = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({  data: bytecode, arguments: ['Hello World'] })
+    .send({from: accounts[0], gas:'1000000'})
+//  console.log(accounts);
 });
 
 describe('Greetings', () => {
-  it('dummy test', () => {
+
+  it('deploys a greetings contract', () => {
+    console.log(greetings);
+    assert.ok(greetings.options.address); // hold the reference to our greetings contract instance, options / address property where we deployed the contract
+  });
+
+  it('has a default message', async () => {
+    const message = await greetings.methods.message().call();
+    assert.equal(message, 'Hello World')
+  });
+
+
+  it('can change the message', async () => {
+    await greetings.methods.setMessage('Hello Avengers').send({ from: accounts[0] } )
+
+    const message = await greetings.methods.message().call();
+
+    assert.equal(message, 'Hello Avengers');
+
   });
 });
